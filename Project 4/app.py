68868bfac2f57c6439a675f1c0e34a503b3bb51f
Ruben Genao <rubengenao@Rubens-MBP.lan>
2022-07-17 20:34:26 -0400

    a bit more progress on my section(stockinfo)

--- Project 4/app.py
+++ Project 4/app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import RegistrationForm
+from stockinfo import getinfo
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2c27df9a9141cf1529a88ca4da79adf3'
 random_list = [1 ,2 ,3 ,4 ,5, 69, 'end']
 
 @app.route('/')
 def index():
     return render_template('home.html', random = random_list)
 
-@app.route('/stocksearch.html')
+@app.route('/stocksearch.html', methods = ["POST", "GET"])
 def search():
-    return render_template('stocksearch.html')
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        print("hi")
+        #flash(f"Search on {form.ticker.data} was successful!", 'success')
+        ticker = form.ticker.data.upper()
+        info = getinfo(ticker)
+        return render_template('stocksearch.html', form = form, infodict = info)        
+    
+    return render_template('stocksearch.html', form = form)
 
 @app.route('/register.html')
 def register():
