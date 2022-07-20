0ec607931e6cf402de1ca6d1cf01fc2a0b98d3aa
Ruben Genao <rubengenao@Rubens-MBP.lan>
2022-07-20 17:20:50 -0400

    Finished with home and stock search deploying to heroku

--- Project 4/app.py
+++ Project 4/app.py
@@ -3,14 +3,18 @@ from tkinter.ttk import LabeledScale
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from stockinfo import getinfo, getlabels, getvalues
+from newsdata import getnews
 
+'''
+y finance, flask, flask wtf, newsdataapi, these are the required imports
+'''
 
-def getpopularstocks(stocklist):
+def getpopularstocks(stocklist, timeperiod, timeinterval = ''):
     stocksinfo = []
     stocks = stocklist
     for stock in stocks:
         stockdict = {}
-        priceaction = getvalues(stock, '1mo')
+        priceaction = getvalues(stock, timeperiod, timeinterval)
 
         trend = ''
         if priceaction[0] <= priceaction[len(priceaction) - 1]:
@@ -20,7 +24,7 @@ def getpopularstocks(stocklist):
         
         percentchange = ((priceaction[len(priceaction) - 1] - priceaction[0]) / priceaction[0]) * 100
         pricechange = priceaction[len(priceaction) - 1] - priceaction[0]
-        stockdict[stock] = {'Date' : getlabels(stock, '1mo'), 'Close' : getvalues(stock, '1mo'), 'Trend' : trend, 'PercentChange' : percentchange, 'PriceChange' : pricechange}
+        stockdict[stock] = {'Date' : getlabels(stock, timeperiod, timeinterval), 'Close' : getvalues(stock, timeperiod, timeinterval), 'Trend' : trend, 'PercentChange' : percentchange, 'PriceChange' : pricechange}
         stocksinfo.append(stockdict)
     return stocksinfo
 
@@ -30,9 +34,10 @@ app.config['SECRET_KEY'] = '2c27df9a9141cf1529a88ca4da79adf3'
 
 @app.route('/')
 def index():
-    index = getpopularstocks(['^IXIC', '^GSPC', '^DJI'])
-    stocks = getpopularstocks(['NFLX', 'AAPL', 'AMC', 'GME'])
-    return render_template('home.html', majorindexes = index, stocklists = stocks)
+    index = getpopularstocks(['^IXIC', '^GSPC', '^DJI'], '1d', '1m')
+    stocks = getpopularstocks(['NFLX', 'AAPL', 'AMC', 'GME'], '1d', '1m')
+    news = getnews()
+    return render_template('home.html', majorindexes = index, stocklists = stocks, stocknews = news)
 
 @app.route('/stocksearch.html', methods = ["POST", "GET"])
 def search():
@@ -43,13 +48,23 @@ def search():
         #flash(f"Search on {form.ticker.data} was successful!", 'success')
         ticker = form.ticker.data.upper()
         info = getinfo(ticker)
-        
+
+        #TODO: Add time span and interval
+
         labels = getlabels(ticker, '1mo')
         values = getvalues(ticker, '1mo')
-        print(labels)
-        print(values)
 
-        return render_template('stocksearch.html', form = form, infodict = info, labels = labels, values = values)        
+        trend = ''
+        if values[0] <= values[len(values) - 1]:
+            trend = 'Up'
+        else:
+            trend = 'Down'
+        
+        percentchange = ((values[len(values) - 1] - values[0]) / values[0]) * 100
+        pricechange = values[len(values) - 1] - values[0]
+
+        return render_template('stocksearch.html', form = form, infodict = info, labels = labels, values = values, 
+                                trend = trend, percentchange = percentchange, pricechange = pricechange)        
     
     return render_template('stocksearch.html', form = form)
 
@@ -63,9 +78,10 @@ def login():
 
 @app.route('/home.html')
 def home():
-    index = getpopularstocks(['^IXIC', '^GSPC', '^DJI'])
-    stocks = getpopularstocks(['NFLX', 'AAPL', 'AMC', 'GME'])
-    return render_template('home.html', majorindexes = index, stocklists = stocks)
+    index = getpopularstocks(['^IXIC', '^GSPC', '^DJI'], '1d', '1m')
+    stocks = getpopularstocks(['NFLX', 'AAPL', 'AMC', 'GME'], '1d', '1m')
+    news = getnews()
+    return render_template('home.html', majorindexes = index, stocklists = stocks, stocknews = news)
 
 if __name__ == '__main__':
     app.run(port=0, debug = True)
