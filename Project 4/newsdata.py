b913ed09fc56380b4d20bd49c4f24e44a876d695
RubenG1019 <90107434+RubenG1019@users.noreply.github.com>
2022-07-29 11:58:38 -0400

    Delete Project 4 directory

--- Project 4/newsdata.py
+++
@@ -1,9 +0,0 @@
-from newsdataapi import NewsDataApiClient
-
-api = NewsDataApiClient(apikey = "pub_93536ec1f0b8d3847ada7bb60eedb0a2a7f6")
-
-def getnews():
-    response = api.news_api (q = 'stock market', country = "us")
-    return response['results']
-
-
