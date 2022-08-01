9e9e42525391f019890a0b2b0276e3417be65c6c
RubenG1019 <90107434+RubenG1019@users.noreply.github.com>
2022-08-01 14:39:36 -0400

    Delete Project 4 directory

--- Project 4/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-class RegistrationForm(FlaskForm):
-    ticker = StringField('Ticker Symbol', validators=[DataRequired(), Length(min=1, max= 6)])
-    submit = SubmitField('Search Ticker')
-    
