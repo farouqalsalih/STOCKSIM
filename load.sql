9e6a1d42cb19f1fe14fe85bae9cc1f21746d737e
Farouq Alsalih <farouqalsalih@gmail.com>
2023-04-07 13:56:52 -0400

    user table created

---
+++ load.sql
@@ -0,0 +1,9 @@
+CREATE TABLE USER (
+    user_id INT NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL, 
+    phone_number VARCHAR(20) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
