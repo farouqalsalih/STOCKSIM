0e95dd1f8f58443373ae84e4edf75aacd8d93903
Farouq Alsalih <farouqalsalih@gmail.com>
2023-04-10 16:27:21 -0400

    Changed SQL db slightly and added some functionality w/ trade.py

--- load.sql
+++ load.sql
@@ -1,11 +1,11 @@
 CREATE TABLE USERS (
     user_id INT NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL, 
     cash float(0) NOT NULL, 
-    phone_number VARCHAR(20) NOT NULL,
-    user_password VARCHAR(20) NOT NULL
+    phone_number VARCHAR(20) NOT NULL UNIQUE,
+    user_password VARCHAR(20) NOT NULL,
     PRIMARY KEY (user_id)
 );
 
@@ -17,13 +17,14 @@ CREATE TABLE DIVIDENDS (
 );
 
 CREATE TABLE ORDERS (
-    order_id INT NOT NULL, 
+    order_id INT AUTO_INCREMENT NOT NULL, 
     user_id INT NOT NULL,
     ticker VARCHAR(50) NOT NULL, 
     buy BOOL NOT NULL, 
     shares INT NOT NULL, 
     price FLOAT(0) NOT NULL, 
-    executed BOOL NOT NULL,
+    executed INT NOT NULL,
+    order_date DATETIME NOT NULL,
     PRIMARY KEY (order_id),
     FOREIGN KEY (ticker) REFERENCES DIVIDENDS(ticker),
     FOREIGN KEY (user_id) REFERENCES USERS(user_id)
@@ -47,3 +48,26 @@ CREATE TABLE PORTFOLIOS (
     FOREIGN KEY (user_id) REFERENCES USERS(user_id),
     FOREIGN KEY (ticker) REFERENCES DIVIDENDS(ticker)
 );
+
+
+DELIMITER //
+CREATE TRIGGER check_and_update_funds_before_buy_order
+BEFORE INSERT ON ORDERS
+FOR EACH ROW
+BEGIN
+    IF NEW.buy = 1 THEN
+        DECLARE user_cash FLOAT;
+        SELECT cash INTO user_cash FROM USERS WHERE user_id = NEW.user_id;
+        
+        IF user_cash < (NEW.shares * NEW.price) THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Insufficient funds for buy order';
+        ELSE
+            UPDATE USERS
+            SET cash = cash - (NEW.shares * NEW.price)
+            WHERE user_id = NEW.user_id;
+        END IF;
+    END IF;
+END;
+//
+DELIMITER ;
+
---
+++ trade.py
@@ -0,0 +1,51 @@
+import mysql.connector
+
+def get_database_connection():
+    return mysql.connector.connect(
+        host='Farouqs-MacBook-Air.local',
+        database='stocksim',
+        user='root',
+        password='Fifoking2!$!',
+        autocommit=False
+    )
+
+def signUp(user_id, email, first, last, cash, phone, user_password):
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    success
+
+    try:
+        cursor.execute(f"INSERT INTO USERS (user_id, email, first_name, last_name, cash, phone_number, user_password) VALUES ({str(user_id)}, '{email}', '{first}', '{last}', {str(cash)}, '{phone}', '{user_password}');")
+        connection.commit()
+        success = False
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        success = True
+    finally:
+        cursor.close()
+        connection.close()
+        return success
+
+def createOrder(user_id, ticker, buy, shares, price):
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("""
+                INSERT INTO ORDERS (user_id, ticker, buy, shares, price, executed)
+                VALUES (%s, %s, %s, %s, %s, 0)
+            """, (user_id, ticker, buy, shares, price))
+
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        return
+
+    if buy:
+        execute_buy_order(user_id, cursor.lastrowid, ticker, shares, price)
+    else:
+        execute_sell_order(user_id, cursor.lastrowid, ticker, shares, price)
+
+def execute_buy_order(user_id, order_id, ticker, shares, price):
+    return
+
+def execute_sell_order(user_id, order_id, ticker, shares, price):
